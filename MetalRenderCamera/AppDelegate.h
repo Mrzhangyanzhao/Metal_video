ffaf84ec8bd1a14c2f5e0e6b07753fc79cdba6df
yz <819274244@qq.com>
2020-09-02 15:22:09 +0800

    Metal-video

---
+++ MetalRenderCamera/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MetalRenderCamera
+//
+//  Created by yz on 2020/8/28.
+//  Copyright © 2020 yz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ MetalRenderCamera/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  MetalRenderCamera
+//
+//  Created by yz on 2020/8/28.
+//  Copyright © 2020 yz. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+
+
+@end
